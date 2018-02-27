22d6793d008e66eac1a673fba385e8a6e530ca4e
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-02-27 19:34:21 +0100

    Third test passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,11 @@
 class Bottles
-  def verse(bottles)
-    "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\n" +
-    "Take one down and pass it around, #{bottles - 1} bottles of beer on the wall.\n"
+  def verse(number)
+    if number == 2
+      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
+      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+    else
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
+      "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
+    end
   end
 end
