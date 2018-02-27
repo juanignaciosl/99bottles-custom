61e1f7e770ae22d6b026eba9816d77495ebd7ecb
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-02-27 20:27:40 +0100

    Fifth test passing

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -1,6 +1,9 @@
 class Bottles
   def verse(number)
     case number
+    when 0
+      "No more bottles of beer on the wall, no more bottles of beer.\n" +
+      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\n" +
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
