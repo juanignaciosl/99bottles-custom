86a5bf8e86e4067ea904e8f73e74e8248fa97ff8
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-02-28 21:15:19 +0100

    Verse 2 no longer needed

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -11,9 +11,6 @@ class Bottles
     when 1
       "1 bottle of beer on the wall, 1 bottle of beer.\n" +
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
-    when 2
-      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
-      "Take one down and pass it around, #{number - 1} #{container(number - 1)} of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
       "Take one down and pass it around, #{number - 1} #{container(number - 1)} of beer on the wall.\n"
