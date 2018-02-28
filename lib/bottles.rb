12406ef5cbb8281aaacb722e2b265ffbe07b7496
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-02-28 21:54:44 +0100

    Quantity

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -10,10 +10,10 @@ class Bottles
       "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
     when 1
       "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\n" +
-      "Take #{pronoun(number)} down and pass it around, no more bottles of beer on the wall.\n"
+      "Take #{pronoun(number)} down and pass it around, #{quantity(number - 1)} bottles of beer on the wall.\n"
     else
       "#{number} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\n" +
-      "Take #{pronoun(number)} down and pass it around, #{number - 1} #{container(number - 1)} of beer on the wall.\n"
+      "Take #{pronoun(number)} down and pass it around, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
 
@@ -28,10 +28,10 @@ class Bottles
    end
 
    def pronoun(number)
-     if number == 1
-       'it'
-     else
-       'one'
-     end
+     number == 1 ? 'it' : 'one'
+   end
+
+   def quantity(number)
+     number == 0 ? "no more" : number
    end
 end
