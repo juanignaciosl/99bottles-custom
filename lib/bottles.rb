76eab5f4f8daf5b929260309d084e525225ff99a
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-02-28 21:12:12 +0100

    Straightforward verse 2 refactor, hardcoded with variable

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -12,8 +12,8 @@ class Bottles
       "1 bottle of beer on the wall, 1 bottle of beer.\n" +
       "Take it down and pass it around, no more bottles of beer on the wall.\n"
     when 2
-      "2 bottles of beer on the wall, 2 bottles of beer.\n" +
-      "Take one down and pass it around, 1 bottle of beer on the wall.\n"
+      "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
+      "Take one down and pass it around, #{number - 1} bottle of beer on the wall.\n"
     else
       "#{number} bottles of beer on the wall, #{number} bottles of beer.\n" +
       "Take one down and pass it around, #{number - 1} bottles of beer on the wall.\n"
