a570c785bd79b97b769308ad58ec51aabace9cd0
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-03-01 08:12:01 +0100

    action

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,10 +7,10 @@ class Bottles
     case number
     when 0
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{quantity(number)} #{container(number)} of beer.\n" +
-      "Go to the store and buy some more, 99 bottles of beer on the wall.\n"
+      "#{action(number)}, 99 bottles of beer on the wall.\n"
     else
       "#{quantity(number).capitalize} #{container(number)} of beer on the wall, #{number} #{container(number)} of beer.\n" +
-      "Take #{pronoun(number)} down and pass it around, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
+      "#{action(number)}, #{quantity(number - 1)} #{container(number - 1)} of beer on the wall.\n"
     end
   end
 
@@ -30,5 +30,13 @@ class Bottles
 
    def quantity(number)
      number == 0 ? "no more" : number.to_s
+   end
+
+   def action(number)
+     if number == 0
+       "Go to the store and buy some more"
+     else
+       "Take #{pronoun(number)} down and pass it around"
+     end
    end
 end
