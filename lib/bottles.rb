6a0356b5af13bc5044cc573c092f5b65cfc87f89
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-09-12 19:46:50 +0200

    Next successor caching

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -5,35 +5,14 @@ class Bottles
 
   def verse(number)
     bottle_number = BottleNumber.new(number)
+    next_bottle_number = BottleNumber.new(bottle_number.successor)
 
     "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, #{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
-    "#{bottle_number.action}, #{quantity(successor(number))} #{container(successor(number))} of beer on the wall.\n"
+      "#{bottle_number.action}, #{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
   end
 
    def song
      verses(99, 0)
-   end
-
-   private
-
-   def container(number)
-     BottleNumber.new(number).container
-   end
-
-   def pronoun(number)
-     BottleNumber.new(number).pronoun
-   end
-
-   def quantity(number)
-     BottleNumber.new(number).quantity
-   end
-
-   def action(number)
-     BottleNumber.new(number).action
-   end
-
-   def successor(number)
-     BottleNumber.new(number).successor
    end
 end
 
