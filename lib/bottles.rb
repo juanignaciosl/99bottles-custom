2d62be6ce29e14d83c27604cdb40596f3506e828
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-09-12 21:58:22 +0200

    Removed quantity+container data clump

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,8 +7,8 @@ class Bottles
     bottle_number = BottleNumber.new(number)
     next_bottle_number = BottleNumber.new(bottle_number.successor)
 
-    "#{bottle_number.quantity.capitalize} #{bottle_number.container} of beer on the wall, #{bottle_number.quantity} #{bottle_number.container} of beer.\n" +
-      "#{bottle_number.action}, #{next_bottle_number.quantity} #{next_bottle_number.container} of beer on the wall.\n"
+    "#{bottle_number.to_s.capitalize} of beer on the wall, #{bottle_number} of beer.\n" +
+    "#{bottle_number.action}, #{next_bottle_number} of beer on the wall.\n"
   end
 
    def song
@@ -61,5 +61,9 @@ class BottleNumber
      else
        number - 1
      end
+   end
+
+   def to_s
+     "#{quantity} #{container}"
    end
 end
