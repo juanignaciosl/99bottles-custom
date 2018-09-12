776dde0d394d65be4a3d3ef56ab4aa774e7922e8
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-09-12 22:11:47 +0200

    Bottle number 0

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -4,8 +4,8 @@ class Bottles
   end
 
   def verse(number)
-    bottle_number = BottleNumber.new(number)
-    next_bottle_number = BottleNumber.new(bottle_number.successor)
+    bottle_number = bottle_number_for(number)
+    next_bottle_number = bottle_number_for(bottle_number.successor)
 
     "#{bottle_number.to_s.capitalize} of beer on the wall, #{bottle_number} of beer.\n" +
     "#{bottle_number.action}, #{next_bottle_number} of beer on the wall.\n"
@@ -13,6 +13,16 @@ class Bottles
 
    def song
      verses(99, 0)
+   end
+
+   private
+
+   def bottle_number_for(number)
+     if number == 0
+       BottleNumber0
+     else
+       BottleNumber
+     end.new(number)
    end
 end
 
@@ -40,30 +50,32 @@ class BottleNumber
    end
 
    def quantity
-     if number == 0
-       'no more'
-     else
-       number.to_s
-     end
+     number.to_s
    end
 
    def action
-     if number == 0
-       "Go to the store and buy some more"
-     else
-       "Take #{pronoun} down and pass it around"
-     end
+     "Take #{pronoun} down and pass it around"
    end
 
    def successor
-     if number == 0
-       99
-     else
-       number - 1
-     end
+     number - 1
    end
 
    def to_s
      "#{quantity} #{container}"
    end
+end
+
+class BottleNumber0 < BottleNumber
+  def quantity
+    'no more'
+  end
+
+  def action
+    "Go to the store and buy some more"
+  end
+  
+  def successor
+    99
+  end
 end
