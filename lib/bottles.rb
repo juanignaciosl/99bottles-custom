1b12d9218e772967db5aabdecc85f619a3755e6e
Juan Ignacio Sánchez Lara <juanignaciosl@gmail.com>
2018-09-12 09:01:49 +0200

    BottleNumber wired

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -20,6 +20,59 @@ class Bottles
      else
        "bottles"
      end
+     BottleNumber.new(number).container(number)
+   end
+
+   def pronoun(number)
+     if number == 1
+       'it'
+     else
+       'one'
+     end
+     BottleNumber.new(number).pronoun(number)
+   end
+
+   def quantity(number)
+     if number == 0
+       'no more'
+     else
+       number.to_s
+     end
+     BottleNumber.new(number).quantity(number)
+   end
+
+   def action(number)
+     if number == 0
+       "Go to the store and buy some more"
+     else
+       "Take #{pronoun(number)} down and pass it around"
+     end
+     BottleNumber.new(number).action(number)
+   end
+
+   def successor(number)
+     if number == 0
+       99
+     else
+       number - 1
+     end
+     BottleNumber.new(number).successor(number)
+   end
+end
+
+class BottleNumber
+   attr_reader:number
+
+   def initialize(number)
+     @number = number
+   end
+
+   def container(number)
+     if number == 1
+       "bottle"
+     else
+       "bottles"
+     end
    end
 
    def pronoun(number)
